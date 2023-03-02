d8b36914f818f17c81ff2b3e540084fc8dbe12ba
surbaniec <radsler1999@gmail.com>
2023-03-02 13:35:57 +0100

    Add routes, create react router, create empty pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import FaqsContainer from './containers/faqs';
-import FooterContainer from './containers/footer';
-import { JumbotronContainer } from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Browse from './pages/browse';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
 function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path={ROUTES.BROWSE} element={<Browse />} />
+        <Route exact path={ROUTES.SIGN_IN} element={<SignIn />} />
+        <Route exact path={ROUTES.SIGN_UP} element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
