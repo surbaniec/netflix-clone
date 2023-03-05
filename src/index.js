bbc94b5260b43edc9022ba36290ff6fa340f73c7
surbaniec <radsler1999@gmail.com>
2023-03-05 16:13:03 +0100

    add firebaseContext

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from './App';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles';
 import { firebaseConnection } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebaseConnection }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>
 );
