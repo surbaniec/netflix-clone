4c7630e505929abdb8d6a580a8a9f6b8439fae00
surbaniec <radsler1999@gmail.com>
2023-03-19 17:54:40 +0100

    add jumbotron, loading, opt-form, profiles, loading tests

---
+++ src/__tests__/components/jumbotron.test.js
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import Jumbotron from '../../components/jumbotron';
+import jumboData from '../../fixtures/jumbo.json';
+
+describe('<Jumbotron />', () => {
+  it('renders the <Jumbtron /> with populated data', () => {
+    const { container, getByText, getByAltText, getByTestId } = render(
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image
+                src={item.image}
+                alt={item.alt}
+                data-testid={`${item.id}-jumbo-image}`}
+              />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    );
+
+    expect(getByText('Enjoy on your TV.')).toBeTruthy();
+    expect(getByAltText('Tiger King on Netflix')).toBeTruthy();
+    expect(getByTestId('1-jumbo-image')).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
